a6a742a5b047d8943b2ab503be814901a2d8fb43
Nan Wu <32279235+virus0071@users.noreply.github.com>
2018-07-27 00:10:32 -0400

    Add files via upload

---
+++ server.js
@@ -0,0 +1,22 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require('path');
+var friends = require('./app/data/friends.js');
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+app.use(express.static('app/public'));
+
+require('./app/routing/apiRoutes.js')(app);
+require('./app/routing/htmlRoutes.js')(app);
+
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+  });
+  
