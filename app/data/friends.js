7f21c5126c2be4e3b56d44246613bb27da2b834c
Nan Wu <32279235+virus0071@users.noreply.github.com>
2018-07-27 00:11:24 -0400

    Create friends.js

---
+++ app/data/friends.js
@@ -0,0 +1,89 @@
+var friends = [
+    {
+        "name": "Adam",
+        "photo": "http://pic.cdhdky.com/download/20170522_134548451.jpg",
+        "scores": [5,2,3,5,1,1,4,4,2,3]
+    },{
+        "name": "Alex",
+        "photo": "http://img.116s.com/2015-03/16/14264668980002.jpg",
+        "scores": [4,3,2,2,1,2,2,1,2,3]
+    },{
+        "name": "Adison",
+        "photo": "http://p.kengdie.com/touxiang/1611/40/1688345.jpg",
+        "scores": [5,2,5,5,5,1,3,5,2,4]
+    },{
+        "name": "Carrey",
+        "photo": "http://p.kengdie.com/touxiang/1608/56/201351440.jpg",
+        "scores": [4,3,2,3,3,2,4,5,3,1]
+    },{
+        "name": "Denny",
+        "photo": "http://images.liqucn.com/img/h1/h994/img201802021545480_info300X300.jpg",
+        "scores": [1,1,3,2,3,2,2,5,2,3]
+    },{
+        "name": "Ellean",
+        "photo": "http://p.kengdie.com/touxiang/1611/40/1688351.jpg",
+        "scores": [5,5,5,2,3,5,4,5,1,2]
+    },{
+        "name": "Gothom",
+        "photo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTxiUNsheiLbQFD-_TNl_rhbVnQ6AiKv0w3U2rVGbLwhHQ9tDLj",
+        "scores": [4,4,3,5,2,4,5,2,4,2]
+    },{
+        "name": "Happy",
+        "photo": "http://www.qq22.com.cn/uploads/allimg/201608192030/u3hi2ihde0s129828.jpg",
+        "scores": [5,5,3,2,4,4,4,4,1,2]
+    },{
+        "name": "Halley",
+        "photo": "http://static.tumblr.com/9vdl7pv/SdAly1o0e/jackson.jpg",
+        "scores": [4,2,3,3,4,4,5,5,2,1]
+    },{
+        "name": "Joan",
+        "photo": "http://p.kengdie.com/touxiang/1611/33/13207939.jpg",
+        "scores": [3,3,3,1,2,2,3,5,4,1]
+    },{
+        "name": "Jane",
+        "photo": "http://www.gx8899.com/uploads/allimg/2017022316/tjpengglhlp.jpg",
+        "scores": [5,2,4,3,5,4,1,2,3,4]
+    },{
+        "name": "Jothan",
+        "photo": "http://tx.haiqq.com/uploads/allimg/170505/00242Lc1-9.jpg",
+        "scores": [4,4,2,3,4,2,1,3,4,5]
+    },{
+        "name": "Kelly",
+        "photo": "http://img.116s.com/2015-11/29/14487792300008.jpg",
+        "scores": [1,2,2,2,2,2,4,2,3,4]
+    },{
+        "name": "Kevin",
+        "photo": "http://www.fzlkz.com/uploads/allimg/c151127/144U960K1P30-42057.jpg",
+        "scores": [1,2,4,2,3,4,5,1,2,3]
+    },{
+        "name": "Leona",
+        "photo": "http://www.qqzhi.com/uploadpic/2014-11-04/101315575.jpg",
+        "scores": [5,2,3,4,5,3,2,4,1,2]
+    },{
+        "name": "Lith",
+        "photo": "http://p.kengdie.com/touxiang/1611/45/162338205.jpg",
+        "scores": [2,2,4,2,3,5,4,1,2,3]
+    },{
+        "name": "Monica",
+        "photo": "http://img.jf258.com/uploads/2014-09-04/211419681.jpg",
+        "scores": [1,2,3,1,1,2,3,3,5,4]
+    },{
+        "name": "Nolan",
+        "photo": "http://tx.haiqq.com/uploads/allimg/170507/0640562411-2.jpg",
+        "scores": [2,3,2,5,2,5,3,5,1,4]
+    },{
+        "name": "Petty",
+        "photo": "http://img.touxiangzhan.com/upload/image/2a4235584654n3842063642t27.jpg",
+        "scores": [3,2,4,5,5,4,5,4,5,4]
+    },{
+        "name": "Rey",
+        "photo": "http://pic5.qqmofasi.com/2015/01/15/506_KuS5RCj57j00L6lzzy0k_square.jpg",
+        "scores": [2,3,2,2,3,3,3,2,4,5]
+    },{
+        "name": "Venus",
+        "photo": "http://www.fzlkz.com/uploads/allimg/c150831/144101354YSF-E120.jpg",
+        "scores": [2,4,4,1,2,3,2,3,5,4]
+    },
+]
+
+module.exports = friends;
